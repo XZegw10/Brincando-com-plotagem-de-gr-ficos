c70c411367d7692538eb405082fc8f7fe9ac4c4c
arthurvasconcelos707 <arthurvasconcelos707@gmail.com>
2025-08-19 22:17:14 +0000

    Mais uma tentativa de fazer o php funcionar, agora configurei as extensões dentro do .nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -22,6 +22,10 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "DEVSENSE.composer-php-vscode"
+      "DEVSENSE.intelli-php-vscode"
+      "DEVSENSE.phptools-vscode"
+      "DEVSENSE.profiler-php-vscode"
     ];
 
     # Enable previews
